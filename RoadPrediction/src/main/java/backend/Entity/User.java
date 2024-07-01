6e7ff5fcad88c31efd23dd9e37756ce4c7d498d7
Edwardsamaxl <578827657@qq.com>
2024-07-01 16:27:28 +0800

    1.0

--- RoadPrediction/src/main/java/backend/Entity/User.java
+++ RoadPrediction/src/main/java/backend/Entity/User.java
@@ -4,6 +4,7 @@ public class User {
     private int id;
     private String username;
     private String password;
+    private String mail;
     public User(int id, String username, String password) {
         this.id = id;
         this.username = username;
@@ -26,5 +27,11 @@ public class User {
     }
     public void setPassword(String password) {
         this.password = password;
+    }
+    public String getMail() {
+        return mail;
+    }
+    public void setMail(String mail) {
+        this.mail = mail;
     }
 }
--- RoadPrediction/src/main/java/backend/Service/UserService.java
+++ RoadPrediction/src/main/java/backend/Service/UserService.java
@@ -1,44 +1,24 @@
 package backend.Service;
 
-import backend.Entity.User;
-import org.springframework.stereotype.Service;
+import jakarta.servlet.http.HttpSession;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+public interface UserService {
+    /**
+     * 用户注册
+     *
+     * @param loginName
+     * @param password
+     * @return
+     */
+    String register(String loginName, String password);
 
-@Service
-public class UserService {
-
-    private final List<User> users = new ArrayList<>();
-    private int nextId = 1;
-
-    public User registerUser(String username, String password) {
-        User newUser = new User(nextId, username, password);
-        users.add(newUser);
-        nextId++;
-        return newUser;
-    }
-
-    public Optional<User> getUserById(int id) {
-        return users.stream()
-                .filter(user -> user.getId() == id)
-                .findFirst();
-    }
-
-    public Optional<User> getUserByUsername(String username) {
-        return users.stream()
-                .filter(user -> user.getUsername().equals(username))
-                .findFirst();
-    }
-
-    public boolean authenticateUser(String username, String password) {
-        Optional<User> userOptional = users.stream()
-                .filter(user -> user.getUsername().equals(username))
-                .findFirst();
-        return userOptional.isPresent() && userOptional.get().getPassword().equals(password);
-    }
-
-    // 可以添加更多的业务方法，如更新用户信息、删除用户等
-
+    /**
+     * 登录
+     *
+     * @param loginName
+     * @param passwordMD5
+     * @param httpSession
+     * @return
+     */
+    String login(String loginName, String passwordMD5, HttpSession httpSession);
 }
